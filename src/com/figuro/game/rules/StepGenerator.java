68f873f019dfb5081f1feece5d2dabf824be3d6c
kalozka1 <kalozka1@gmail.com>
2015-05-03 13:41:58 +0300

    Remove unused imports

--- src/com/figuro/game/rules/StepGenerator.java
+++ src/com/figuro/game/rules/StepGenerator.java
@@ -9,8 +9,6 @@ import com.figuro.common.BoardState;
 import com.figuro.common.Cell;
 import com.figuro.common.ICell;
 import com.figuro.common.IUnit;
-import com.figuro.common.UnitEnum;
-import com.sun.xml.internal.ws.dump.LoggingDumpTube;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
