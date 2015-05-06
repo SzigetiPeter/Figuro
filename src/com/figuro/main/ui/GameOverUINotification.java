eaf256271d621d3e8d8e9ea32c5377db79850ec5
Máthé E. Botond <dev@qwerty.ms>
2015-05-06 18:23:46 +0300

    added GameOverCallback stub

---
+++ src/com/figuro/main/ui/GameOverUINotification.java
@@ -0,0 +1,21 @@
+package com.figuro.main.ui;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Dialog;
+
+import com.figuro.engine.IGameoverCallback;
+
+public class GameOverUINotification implements IGameoverCallback {
+	private Scene scene;
+
+	public GameOverUINotification(Scene scene) {
+		this.scene = scene;
+	}
+	
+	@Override
+	public void gameFinishedWith(String score) {
+		//1. display dialog with message: score
+		//2. Ok button with event to showStartScreen()
+	}
+
+}
