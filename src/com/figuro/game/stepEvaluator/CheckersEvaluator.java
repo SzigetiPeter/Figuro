9fa527c798cfc3d963b7d303747156441dbf5558
kalozka1 <kalozka1@gmail.com>
2015-05-10 19:49:40 +0300

    Add getMinimum and getMaximum to StepEvaluator.

--- src/com/figuro/game/stepEvaluator/CheckersEvaluator.java
+++ src/com/figuro/game/stepEvaluator/CheckersEvaluator.java
@@ -17,62 +17,71 @@ import com.figuro.game.rules.UnitEnum;
  */
 public class CheckersEvaluator implements IStepEvaluator {
 
-	private IMinimumCalculator minimumCalculator;
+    private IMinimumCalculator minimumCalculator;
+    
+    public CheckersEvaluator()
+    {
+        minimumCalculator = new MinimumCalculator();
+    }
+    
+    @Override
+    public Evaluation evaluate(BoardState state, int player) {
+        if (state == null)
+            return null;
+        
+        ICell[][] cells = state.getBoard();
+        
+        if (cells == null)
+            return null;
+        
+        Evaluation evaluation = new Evaluation();
+        int score = 0;
+        
+        
+        for (int i = 0; i < cells.length; ++i) {
+            for (int j = 0; j < cells[0].length; ++j) {
+                ICell currentCell = cells[i][j];
+                if (currentCell != null && currentCell.hasUnit())
+                {
+                    IUnit unit = currentCell.getUnit();
+                    
+                    if (unit.getOwnerId() == player)
+                    {
+                        if (unit.getType() == UnitEnum.PEASANT)
+                        {
+                            score += 1;
+                        }
+                        else if (unit.getType() == UnitEnum.KING)
+                        {
+                            score += 3;
+                        }
+                    }
+                }
+            }
+        }
+            
+        evaluation.setPlayer(player);
+        evaluation.setScore(score);
+        
+        return evaluation;
+    }
 
-	public CheckersEvaluator() {
-		minimumCalculator = new MinimumCalculator();
-	}
-
-	@Override
-	public Evaluation evaluate(BoardState state, int player) {
-		if (state == null)
-			return null;
-
-		ICell[][] cells = state.getBoard();
-
-		if (cells == null)
-			return null;
-
-		Evaluation evaluation = new Evaluation();
-		int score = 0;
-
-		for (int i = 0; i < cells.length; ++i) {
-			for (int j = 0; j < cells[0].length; ++j) {
-				ICell currentCell = cells[i][j];
-				if (currentCell != null && currentCell.hasUnit()) {
-					IUnit unit = currentCell.getUnit();
-
-					if (unit.getOwnerId() == player) {
-						if (unit.getType() == UnitEnum.PEASANT) {
-							score += 1;
-						} else if (unit.getType() == UnitEnum.KING) {
-							score += 3;
-						}
-					}
-				}
-			}
-		}
-
-		evaluation.setPlayer(player);
-		evaluation.setScore(score);
-
-		return evaluation;
-	}
-
-	@Override
-	public Evaluation min(Evaluation a, Evaluation b, int player) {
-		return minimumCalculator.min(a, b, player);
-	}
-
-	@Override
-	public Evaluation getMinimum() {
-		// TODO: return the minimum
-		return null;
-	}
-
-	@Override
-	public Evaluation getMaximum() {
-		// TODO: return the maximum
-		return null;
-	}
+    @Override
+    public Evaluation min(Evaluation a, Evaluation b, int player) {
+        return minimumCalculator.min(a, b, player);
+    }
+    
+    @Override
+    public Evaluation getMinimum() {
+    	int lowestScore = 0;
+        Evaluation evaluation = new Evaluation(0, lowestScore);
+    	return evaluation;
+    }
+    
+    @Override
+    public Evaluation getMaximum() {
+        int highestScore = 36; // = 12 puppet * 3 points (all puppets are KING)
+        Evaluation evaluation = new Evaluation(0, highestScore);
+    	return evaluation;
+    }
 }
